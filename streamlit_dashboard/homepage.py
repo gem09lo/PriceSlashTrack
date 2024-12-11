f75bba11b972e9622135ef54e71e4849596396ee
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-12-11 15:49:00 +0000

    Making progress with the login page

--- streamlit_dashboard/homepage.py
+++ streamlit_dashboard/homepage.py
@@ -11,22 +11,40 @@ header_section = st.container(border=True, height=500)
 
 def home_page() -> None:
     """ Function that runs the whole home page. """
-    page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.photowall.com/products/64251/green-trees-in-forest.jpg?h=699&q=85");
-background-size: cover;
-}
-</style>
-'''
 
+    page_bg_img = '''
+    <style>
+    [data-testid="stAppViewContainer"] {
+    background-image: url("https://windybot.com/img/6k9ecnQIon74X6sFLeQc.jpg");
+    background-size: cover;
+    }
+
+    [data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
+    }
+
+    [data-testid="stVerticalBorderWrapper"] {
+    background-image: url("https://cdn.corporatefinanceinstitute.com/assets/cash-ratio.jpg");
+    background-size: cover;
+    }
+    </style>
+    '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
     with st.sidebar:
         st.write("")
-        st.image("logo.png", width=300)
+        st.image("logo.png", use_container_width=True, clamp=True)
     with header_section:
+        container_img = '''
+        <style>
+        [data-testid="stVerticalBorderWrapper"] {
+        background-image: url("https://cdn.corporatefinanceinstitute.com/assets/cash-ratio.jpg");
+        background-size: cover;
+        }
+        </style>
+        '''
+        st.markdown(container_img, unsafe_allow_html=True)
         st.markdown(
-            "<h1 style='text-align: center; color: green;'>Price Slashers Pipeline</h1>", unsafe_allow_html=True)
+            "<h1 style='text-align: center; color: #ebffe1;'>Price Slashers Pipeline</h1>", unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
