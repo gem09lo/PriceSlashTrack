2d6523ac907901e35ec0c07c86d0267eae840993
Jeevan <trainee.jeevan.george@sigmalabs.co.uk>
2024-12-12 15:32:53 +0000

    Started looking through html to setup a 'Special Deals tab'

--- streamlit_dashboard/homepage.py
+++ streamlit_dashboard/homepage.py
@@ -4,18 +4,54 @@ Creating a home page to greet customers for our dashboard.
 
 """
 import streamlit as st
-
-st.set_page_config(layout="wide")
-header_section = st.container(border=True, height=500)
+import requests
+import streamlit.components.v1 as components
 
 
 def home_page() -> None:
     """ Function that runs the whole home page. """
+    st.logo("logo.png", size='large')
 
+    initial_banner()
+    st.container(height=2, border=False)
+    information_section()
+
+
+def meet_us_button():
+    with st.popover(label="Meet Us!"):
+        st.markdown("Ahahaha")
+
+
+def information_section():
+    """Adding size to buttons"""
+    st.markdown("""
+    <style>
+                
+    .stButton>button {
+    color: #30693e;
+    border-radius: 10%;
+    height: 5em;
+    width: 10em;
+                
+    }
+    </style>""", unsafe_allow_html=True)
+    c = st.columns(7)
+    with c[1]:
+        st.button("Meet Us")
+    with c[3]:
+        st.button("Get Started!")
+    with c[5]:
+        st.button("Help Us!")
+
+    components.iframe("https://store.steampowered.com/")
+
+
+def initial_banner() -> None:
+    """ Top Banner """
     page_bg_img = '''
     <style>
     [data-testid="stAppViewContainer"] {
-    background-image: url("https://windybot.com/img/6k9ecnQIon74X6sFLeQc.jpg");
+    background-image: url("https://static.vecteezy.com/system/resources/previews/002/323/313/non_2x/banknotes-rain-money-falling-vector.jpg");
     background-size: cover;
     }
 
@@ -27,25 +63,24 @@ def home_page() -> None:
     background-image: url("https://cdn.corporatefinanceinstitute.com/assets/cash-ratio.jpg");
     background-size: cover;
     }
+
+    .border-class {
+        border: 2px solid #555
+    }
+
     </style>
     '''
     st.markdown(page_bg_img, unsafe_allow_html=True)
-    with st.sidebar:
-        st.write("")
-        st.image("logo.png", use_container_width=True, clamp=True)
-    with header_section:
-        container_img = '''
-        <style>
-        [data-testid="stVerticalBorderWrapper"] {
-        background-image: url("https://cdn.corporatefinanceinstitute.com/assets/cash-ratio.jpg");
-        background-size: cover;
-        }
-        </style>
-        '''
-        st.markdown(container_img, unsafe_allow_html=True)
-        st.markdown(
-            "<h1 style='text-align: center; color: #ebffe1;'>Price Slashers Pipeline</h1>", unsafe_allow_html=True)
+
+    st.markdown("""
+                <head>
+                <img src='https://i.imgur.com/VJL4LVo.png' class="border-class">
+                </head>
+                """,
+                unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
+    st.set_page_config(page_title="Price Slashers - Sales Tracker",
+                       page_icon="💸", layout="wide")
     home_page()
